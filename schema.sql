6bac4d8be4fd9420643f6228793159bf0f799a4e
lkaruppiah54462 <lakshmazda@gmail.com>
2020-06-20 12:43:10 -0700

    querry and retirement info

--- schema.sql
+++ schema.sql
@@ -44,8 +44,8 @@ CREATE TABLE titles (
 );
 
 CREATE TABLE dept_emp (
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+    dept_no VARCHAR(4) NOT NULL,	
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -54,3 +54,4 @@ FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
 );
 
 SELECT * FROM departments;
+DROP TABLE dept_emp CASCADE;
