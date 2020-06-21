6b1e39f9fe3fba1e32956b69ba95e7d57bb1920a
lkaruppiah54462 <lakshmazda@gmail.com>
2020-06-20 17:35:45 -0700

    challenge

--- querries.sql
+++ querries.sql
@@ -1,4 +1,3 @@
-DROP TABLE dept_emp CASCADE;
 --querries
 SELECT * FROM departments;
 SELECT * FROM employees;
@@ -174,16 +173,82 @@ INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
 INNER JOIN departments AS d
 ON (de.dept_no = d.dept_no)
-WHERE (d.dept_name = 'Sales');
+WHERE (d.dept_name IN('Sales','Development'));
+
 
 SELECT ce.emp_no,
 ce.first_name,
 ce.last_name,
-d.dept_name
--- INTO dept_info
+ti.title,
+de.from_date,
+s.salary
+INTO title_info
 FROM current_emp as ce
 INNER JOIN dept_emp AS de
 ON (ce.emp_no = de.emp_no)
-INNER JOIN departments AS d
-ON (de.dept_no = d.dept_no)
-WHERE (d.dept_name IN('Sales','Development'));
+INNER JOIN titles AS ti
+ON (ce.emp_no = ti.emp_no)
+INNER JOIN salaries AS s
+ON (ce.emp_no = s.emp_no);
+
+--REDONE because of unique key problem with title
+
+SELECT * FROM titles
+
+
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+SELECT ri.emp_no,
+	ri.first_name,
+	ri.last_name,
+	de.to_date
+INTO current_emp
+FROM retirement_info as ri
+LEFT JOIN dept_emp as de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+SELECT ce.emp_no,
+ce.first_name,
+ce.last_name,
+ti.title,
+de.from_date,
+s.salary
+INTO title_info
+FROM current_emp as ce
+INNER JOIN dept_emp AS de
+ON (ce.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (ce.emp_no = ti.emp_no)
+INNER JOIN salaries AS s
+ON (ce.emp_no = s.emp_no);
+
+
+SELECT * FROM title_info
+
+SELECT * FROM titles
+
+SELECT first_name,last_name,count(*) 
+FROM title_info
+GROUP BY
+	first_name,
+	last_name
+HAVING count(*) > 1
+
+SELECT * 
+INTO title_unique_info
+FROM 
+(SELECT *,ROW_NUMBER() OVER
+ (PARTITION BY (emp_no)
+ ORDER BY from_date DESC) rn
+ FROM title_info
+ ) tmp WHERE rn = 1
+ORDER BY emp_no;
+
+SELECT * FROM title_unique_info
